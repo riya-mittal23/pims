efdaa2b5c01b2c4766fcad475a44f758ce52e4fe
riya-mittal23 <riyamittal232@gmail.com>
2026-01-19 17:24:51 +0530

    final code

--- backend/config/MongoDBManager.js
+++ backend/config/MongoDBManager.js
@@ -12,7 +12,7 @@ export const connectDB = async () => {
     }
 
     await mongoose.connect(uri);
-    console.log("MongoDB connected");
+    console.log("MongoDB connected to server");
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
     process.exit(1);
